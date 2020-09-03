3728864c0c8edbbce58025f4c782e3ff00032770
Made Indrayana <indra@doubleshot-audio.com>
2020-09-03 17:55:50 +0200

    Optimizing code. Adding FFmpeg check to prevent program running without a valid environment variable of FFmpeg.

--- DSA-Multitool.cpp
+++ DSA-Multitool.cpp
@@ -4,27 +4,31 @@
 #include <iostream>
 #include <limits>
 #include <sstream>
-#include <string>
+#include <string>       // std::string
+#include <algorithm>    // std::search
+#include <vector>
+#include <windows.h>
 
 
 // Variable Init
-char cStringCheck = '\"';
-std::string sVideoFile = "init";
-std::string sAudioFile = "init";
+char                cStringCheck = '\"';
+std::string         sVideoFile = "init";
+std::string         sAudioFile = "init";
+
 std::string sResultFile(std::string sSourceFile, std::string sAppend)
 {
-    return sSourceFile.insert(sSourceFile.length() - 5, sAppend);
+	return sSourceFile.insert(sSourceFile.length() - 5, sAppend);
 }
 std::string sResultFileNewFormat(std::string sSourceFile, std::string sAppend)
 {
-    sSourceFile.erase(sSourceFile.length() - 5); // erasing the original video file format extension (e.g. .mp4) from the length - 5 to the end
-    return sSourceFile.insert(sSourceFile.length(), sAppend);
+	sSourceFile.erase(sSourceFile.length() - 5); // erasing the original video file format extension (e.g. .mp4) from the length - 5 to the end
+	return sSourceFile.insert(sSourceFile.length(), sAppend);
 }
 
 
 void showTitle()
 {
-    std::cout << R"(
+	std::cout << R"(
         >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
         <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
                                                                                            
@@ -40,139 +44,182 @@ void showTitle()
         >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
         <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 )";
-    std::cout << '\n';
+	std::cout << '\n';
 }
 
 
 void ConvertVideo()
 {
-    std::string sAppend = "-Nuendo";
-    
-    std::cout << "Please drag and drop the video file to convert :\n";
-    std::getline(std::cin, sVideoFile);
+	std::string sAppend = "-Nuendo";
 
-    
-    while ((sVideoFile.at(0) != cStringCheck) || sVideoFile.length() < 4)
-    {
-        std::cout << "ERROR: Invalid input \n" << "Please drag and drop the video file to convert :\n";
-        std::cin.clear();
+	std::cout << "Please drag and drop the video file to convert :\n";
+	std::getline(std::cin, sVideoFile);
 
-        std::getline(std::cin, sVideoFile);
-    }
 
-    system(("ffmpeg -i " + sVideoFile + " -bf 0 -g 1 " + sResultFile(sVideoFile, sAppend)).c_str());
+	while ((sVideoFile.at(0) != cStringCheck) || sVideoFile.length() < 4)
+	{
+		std::cout << "ERROR: Invalid input \n" << "Please drag and drop the video file to convert :\n";
+		std::cin.clear();
+
+		std::getline(std::cin, sVideoFile);
+	}
+
+	system(("ffmpeg -i " + sVideoFile + " -bf 0 -g 1 " + sResultFile(sVideoFile, sAppend)).c_str());
 }
 
 void ExtractAudio()
 {
-    std::string sAppend = "-WavAudio.wav\"";
+	std::string sAppend = "-WavAudio.wav\"";
 
-    std::cout << "Please drag and drop the video file to extract audio track :\n";
-    std::getline(std::cin, sVideoFile);
+	std::cout << "Please drag and drop the video file to extract audio track :\n";
+	std::getline(std::cin, sVideoFile);
 
-    while ((sVideoFile.at(0) != cStringCheck) || sVideoFile.length() < 4)
-    {
-        std::cout << "ERROR: Invalid input \n" << "Please drag and drop the video file to convert :\n";
-        std::cin.clear();
+	while ((sVideoFile.at(0) != cStringCheck) || sVideoFile.length() < 4)
+	{
+		std::cout << "ERROR: Invalid input \n" << "Please drag and drop the video file to convert :\n";
+		std::cin.clear();
 
-        std::getline(std::cin, sVideoFile);
-    }
+		std::getline(std::cin, sVideoFile);
+	}
 
-    system(("ffmpeg -i " + sVideoFile + " -vn -c:a pcm_s24le " + sResultFileNewFormat(sVideoFile, sAppend)).c_str());
+	system(("ffmpeg -i " + sVideoFile + " -vn -c:a pcm_s24le " + sResultFileNewFormat(sVideoFile, sAppend)).c_str());
 }
 
 void CombineVideoAudioWav()
 {
-    std::string sAppend = "-NewAudio.mov\"";
-    
-    std::cout << "Please drag and drop the video file :\n";
-    std::getline(std::cin, sVideoFile);
+	std::string sAppend = "-NewAudio.mov\"";
 
-    while ((sVideoFile.at(0) != cStringCheck) || sVideoFile.length() < 4)
-    {
-        std::cout << "ERROR: Invalid input \n" << "Please drag and drop the video file to convert :\n";
-        std::cin.clear();
+	std::cout << "Please drag and drop the video file :\n";
+	std::getline(std::cin, sVideoFile);
 
-        std::getline(std::cin, sVideoFile);
-    }
+	while ((sVideoFile.at(0) != cStringCheck) || sVideoFile.length() < 4)
+	{
+		std::cout << "ERROR: Invalid input \n" << "Please drag and drop the video file to convert :\n";
+		std::cin.clear();
 
-    std::cout << "\nPlease drag and drop the audio file (WAV only!) : \n";
-    std::getline(std::cin, sAudioFile);
+		std::getline(std::cin, sVideoFile);
+	}
 
-    while ((sAudioFile.at(0) != cStringCheck) || sAudioFile.length() < 4)
-    {
-        std::cout << "ERROR: Invalid input \n" << "Please drag and drop the video file to convert :\n";
-        std::cin.clear();
+	std::cout << "\nPlease drag and drop the audio file (WAV only!) : \n";
+	std::getline(std::cin, sAudioFile);
 
-        std::getline(std::cin, sVideoFile);
-    }
+	while ((sAudioFile.at(0) != cStringCheck) || sAudioFile.length() < 4)
+	{
+		std::cout << "ERROR: Invalid input \n" << "Please drag and drop the video file to convert :\n";
+		std::cin.clear();
 
-    system(("ffmpeg -i " + sVideoFile + " -i " + sAudioFile + " -map 0:v:0 -map 1:a:0 -c:v copy -c:a copy " + sResultFileNewFormat(sVideoFile, sAppend)).c_str());
+		std::getline(std::cin, sVideoFile);
+	}
+
+	system(("ffmpeg -i " + sVideoFile + " -i " + sAudioFile + " -map 0:v:0 -map 1:a:0 -c:v copy -c:a copy " + sResultFileNewFormat(sVideoFile, sAppend)).c_str());
 }
 
 void CombineVideoAudioAac()
 {
-    std::string sVideoFile = "init";
-    std::string sAudioFile = "init";
-    std::string sAppend = "-NewAudio.mp4\"";
+	std::string sVideoFile = "init";
+	std::string sAudioFile = "init";
+	std::string sAppend = "-NewAudio.mp4\"";
 
-    std::cout << "Please drag and drop the video file :\n";
-    std::getline(std::cin, sVideoFile);
-    std::cout << "\nPlease drag and drop the audio file : \n";
-    std::getline(std::cin, sAudioFile);
+	std::cout << "Please drag and drop the video file :\n";
+	std::getline(std::cin, sVideoFile);
+	std::cout << "\nPlease drag and drop the audio file : \n";
+	std::getline(std::cin, sAudioFile);
 
-    system(("ffmpeg -i " + sVideoFile + " -i " + sAudioFile + " -map 0:v:0 -map 1:a:0 -c:v copy -c:a aac " + sResultFileNewFormat(sVideoFile, sAppend)).c_str());
+	system(("ffmpeg -i " + sVideoFile + " -i " + sAudioFile + " -map 0:v:0 -map 1:a:0 -c:v copy -c:a aac " + sResultFileNewFormat(sVideoFile, sAppend)).c_str());
+}
+
+bool CheckFfmpeg()
+{
+	std::string sSearchString = "ffmpeg";
+	char* pValue;
+	size_t len;
+
+	errno_t err = _dupenv_s(&pValue, &len, "path");
+
+	// if (err) return -1; NOT SURE WHY IS THIS HERE?
+
+	std::string sToSearch(pValue);
+
+	std::for_each(sToSearch.begin(), sToSearch.end(), [](char& c)
+		{
+			c = ::tolower(c); // make PATH completely lowercase to be searchable
+		});
+
+	std::size_t found = sToSearch.find(sSearchString);
+	if (found != std::string::npos)
+	{
+		std::cout << "FFmpeg Found!" << '\n' << '\n';
+		free(pValue); // free buffer again
+		return true;
+	}
+
+	else std::cout << "ERROR: FFmpeg not found, please install FFmpeg first." << '\n' << '\n';
+	free(pValue); // free buffer again
+	return false;
+
 }
 
 int main()
 {
-    int iSelection = 0;
+	int iSelection = 0;
 
-    showTitle();
-    
-    std::cout << "                                 Double Shot Audio Multi Tool v0.1\n";
-    std::cout << "                       Only use if you have ffmpeg installed and configured in cmd\n\n";
-    
-    std::cout << "Tools selection: \n";
-    std::cout << "1. Video Converter for Nuendo / Cubase \n";
-    std::cout << "2. Audio Extractor \n";
-    std::cout << "3. Audio Replacer for Video (MOV container with WAV) \n";
-    std::cout << "4. Audio Replacer for Video (MP4 container with AAC) \n\n";
+	showTitle();
 
-    std::cout << "Input your selection : ";
-   
-    while (!(std::cin >> iSelection) || (iSelection < 1 || iSelection > 4)) // Input validation
-    {
-        std::cout << "ERROR: Please enter a valid number : ";
-        std::cin.clear();
-        std::cin.ignore();
-    }
+	std::cout << "                                 Double Shot Audio Multi Tool v0.1\n";
+	std::cout << "                       Only use if you have ffmpeg installed and configured in cmd\n\n";
 
-    std::cin.ignore(); // to ignore the \n character after enter!
-    
-    if (iSelection == 1)
-    {
-        ConvertVideo();
-    }
+	if (CheckFfmpeg())
+	{
 
-    else if (iSelection == 2)
-    {
-        ExtractAudio();
-    }
 
-    else if (iSelection == 3)
-    {
-        CombineVideoAudioWav();
-    }
+		std::cout << "Tools selection: \n";
+		std::cout << "1. Video Converter for Nuendo / Cubase \n";
+		std::cout << "2. Audio Extractor \n";
+		std::cout << "3. Audio Replacer for Video (MOV container with WAV) \n";
+		std::cout << "4. Audio Replacer for Video (MP4 container with AAC) \n\n";
 
-    else if (iSelection == 4)
-    {
-        CombineVideoAudioAac();
-    }
+		std::cout << "Input your selection : ";
 
-    std::cout << "Program has ended. \n";
-    system("pause");
-    return 0;
+		while (!(std::cin >> iSelection) || (iSelection < 1 || iSelection > 5)) // Input validation
+		{
+			std::cout << "ERROR: Please enter a valid number : ";
+			std::cin.clear();
+			std::cin.ignore();
+		}
+
+		std::cin.ignore(); // to ignore the \n character after enter!
+
+		switch (iSelection)
+		{
+		case 1:
+			ConvertVideo();
+			break;
+		case 2:
+			ExtractAudio();
+			break;
+		case 3:
+			CombineVideoAudioWav();
+			break;
+		case 4:
+			CombineVideoAudioAac();
+			break;
+		case 5:
+			CheckFfmpeg();
+			break;
+		}
+
+
+
+		std::cout << "Program has ended. \n";
+		system("pause");
+		return 0;
+	}
+	else
+	{
+		std::cout << "Program has ended. \n";
+		system("pause");
+		return 0;
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
